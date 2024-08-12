f8181b6c41cb8b40c633b703e0ae0cfd5751264e
vagner ventura <vagnerventura@hotmail.com>
2024-08-11 22:41:22 -0300

    first commit

---
+++ src/test/java/com/Vagner/Agregador/AgregadorInvestimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Vagner.Agregador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgregadorInvestimentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
